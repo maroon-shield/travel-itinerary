2b4de40509a8a6ffc1fe139018887f26c8c0b24f
Moe Sato <maroonshield934@gmail.com>
2026-09-04 16:56:26 +0900

    ルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import DayView from "~/pages/DayView";
+import Home from "~/pages/Home";
+
 function App() {
   return (
-    <main className="mx-auto flex min-h-screen max-w-md flex-col bg-[url(src/assets/hero-sample.png)] bg-cover px-5 py-10 text-white">
-      <div className="flex flex-1 flex-col justify-center pb-48">
-        <h1 className="py-2.5 text-6xl">金沢旅行</h1>
-        <p className="px-1.5">2026.09.13 - 2026.09.15</p>
-      </div>
-
-      <button className="rounded-full bg-black p-4 text-center">
-        出発する
-      </button>
-    </main>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/day-view" element={<DayView />} />
+    </Routes>
   );
 }
 
